750299ed6d6972c52830050760f17d3ca110e9dc
Chris Perkins <chris.perkins@intel.com>
2016-09-16 15:16:25 -0700

    sec

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 /*jshint node:true, esversion:6 */
 
+/*
 var express = require('express');
 
 var app     = express();
@@ -16,3 +17,17 @@ app.get('/hello', function (req, res) {
 app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
 });
+*/
+
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+var a = https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(3001);
